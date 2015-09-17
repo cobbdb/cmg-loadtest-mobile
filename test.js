6f48942a232e0f51030e7525177a1a9c014db50c
Dan <cobbdb@gmail.com>
2015-09-17 09:19:41 -0700

    .

--- test.js
+++ test.js
@@ -1,23 +1,26 @@
 var fs = require('fs'),
+    keys = require('./keys.json').keys,
     WebPageTest = require('webpagetest'),
-    wpt = new WebPageTest('www.webpagetest.org', 'A.8ac6121d4e564cada1a270cd7a5d1de1'),
     concurrency = 9,
     runsPerDay = 190 / concurrency,
     msPerDay = 24 * 60 * 60 * 1000,
     msPerRun = msPerDay / runsPerDay;
 
-global.setInterval(function () {
-    wpt.runTest('http://www.atlantacmgsite.com', {
-        runs: concurrency
-    }, function (err, data) {
-        if (err) {
-            return console.log('>!!! WPT ERROR', err);
-        }
-        fs.writeFile('results.log', global.JSON.stringify(data), function (err) {
+keys.forEach(function (key) {
+    var wpt = new WebPageTest('www.webpagetest.org', key);
+    global.setInterval(function () {
+        wpt.runTest('http://www.atlantacmgsite.com', {
+            runs: concurrency
+        }, function (err, data) {
             if (err) {
-                return console.log('>!!! LOG ERROR', err);
+                return console.log('>!!! WPT ERROR', err);
             }
-            console.log('> Started test:\n\t\t', data);
+            fs.writeFile('results.log', global.JSON.stringify(data), function (err) {
+                if (err) {
+                    return console.log('>!!! LOG ERROR', err);
+                }
+                console.log('> Started test:\n\t\t', data);
+            });
         });
-    });
-}, msPerRun);
+    }, msPerRun);
+});
