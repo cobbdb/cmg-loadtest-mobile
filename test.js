264e68229ba6bfc743ae83b02813c9b036211c19
Dan <cobbdb@gmail.com>
2015-09-17 11:01:01 -0700

    .

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
 var fs = require('fs'),
     keys = require('./keys.json').keys,
     WebPageTest = require('webpagetest'),
-    concurrency = 1,
+    concurrency = 8,
     runsPerDay = 195 * keys.length / concurrency,
     msPerDay = 24 * 60 * 60 * 1000,
     msPerRun = msPerDay / runsPerDay,
-    i = 0;
+    i = 0,
+    logRetention = 2;
 
 global.setInterval(function () {
     var wpt = new WebPageTest('www.webpagetest.org', keys[i]);
     wpt.runTest('http://www.atlantacmgsite.com', {
         runs: concurrency
     }, function (err, data) {
-        var msg = global.JSON.stringify(err || data) + '\n\n';
-        if (i === 0) {
+        var msg = global.JSON.stringify(err || data) + '\n\n',
+            newLog = (i % (keys.length * logRetention)) === 0;
+        if (newLog) {
             fs.writeFile('results.log', msg, function (err) {});
         } else {
             fs.appendFile('results.log', msg, function (err) {});
