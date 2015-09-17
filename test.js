d5c886fbc1e8f6875c6b7531f4fce14130dcb1b9
Dan <cobbdb@gmail.com>
2015-09-17 09:54:26 -0700

    .

--- test.js
+++ test.js
@@ -2,25 +2,27 @@ var fs = require('fs'),
     keys = require('./keys.json').keys,
     WebPageTest = require('webpagetest'),
     concurrency = 9,
-    runsPerDay = 190 / concurrency,
+    runsPerDay = 195 * keys.length / concurrency,
     msPerDay = 24 * 60 * 60 * 1000,
-    msPerRun = msPerDay / runsPerDay;
+    msPerRun = msPerDay / runsPerDay,
+    i = 0;
 
-keys.forEach(function (key) {
-    var wpt = new WebPageTest('www.webpagetest.org', key);
-    global.setInterval(function () {
-        wpt.runTest('http://www.atlantacmgsite.com', {
-            runs: concurrency
-        }, function (err, data) {
+global.setInterval(function () {
+    var wpt = new WebPageTest('www.webpagetest.org', keys[i]);
+    wpt.runTest('http://www.atlantacmgsite.com', {
+        runs: concurrency
+    }, function (err, data) {
+        if (err) {
+            return console.log('>!!! WPT ERROR', err);
+        }
+        fs.writeFile('results.log', global.JSON.stringify(data), function (err) {
             if (err) {
-                return console.log('>!!! WPT ERROR', err);
+                return console.log('>!!! LOG ERROR', err);
             }
-            fs.writeFile('results.log', global.JSON.stringify(data), function (err) {
-                if (err) {
-                    return console.log('>!!! LOG ERROR', err);
-                }
-                console.log('> Started test:\n\t\t', data);
-            });
+            console.log('> Started test:\n\t\t', data);
         });
-    }, msPerRun);
-});
+    });
+
+    i += 1;
+    i %= keys.length;
+}, msPerRun);
